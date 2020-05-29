6a4b280d1f1054a503b2f8c46059c0d7cc320b26
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-28 21:14:59 -0700

    Initial commit

---
+++ test/spec/index.test.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+
+var access = require('../..');
+
+describe('fs-access', function () {
+  it('exists', function (done) {
+    access(__filename, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+
+  it('does not exist', function (done) {
+    // eslint-disable-next-line no-path-concat
+    var fullPath = __filename + 'junk';
+    access(fullPath, function (err) {
+      assert.ok(err);
+      assert.equal(err.message.indexOf('ENOENT:'), 0);
+      assert.equal(err.code, 'ENOENT');
+      assert.equal(err.errno, -2);
+      done();
+    });
+  });
+});
