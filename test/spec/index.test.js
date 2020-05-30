0e793b4df02cd5fa76df1ff6a7cbd380ac91c98e
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-30 14:54:40 -0700

    Tweak tests

--- test/spec/index.test.js
+++ test/spec/index.test.js
@@ -12,8 +12,7 @@ describe('fs-access-compat', function () {
 
   it('does not exist', function (done) {
     // eslint-disable-next-line no-path-concat
-    var fullPath = __filename + 'junk';
-    access(fullPath, function (err) {
+    access(__filename + 'junk', function (err) {
       assert.ok(err);
       assert.equal(err.message.indexOf('ENOENT'), 0);
       assert.equal(err.code, 'ENOENT');
