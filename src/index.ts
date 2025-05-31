bd01c495005a00ba09299cb8e4424a72dd5dd13b
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-31 10:28:23 -0700

    Comprehensive types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-function accessError(fullPath) {
+function accessError(fullPath: string): NodeJS.ErrnoException {
   const err = new Error(`ENOENT: no such file or directory, access '${fullPath}'`) as NodeJS.ErrnoException;
   err.code = 'ENOENT';
   err.errno = -2;
@@ -9,7 +9,8 @@ function accessError(fullPath) {
 
 export type NoParamCallback = (err: NodeJS.ErrnoException | null) => void;
 
-export default (fullPath: string, callback: NoParamCallback) =>
+export default (fullPath: string, callback: NoParamCallback): undefined => {
   fs.stat(fullPath, (err) => {
     return err ? callback(accessError(fullPath)) : callback(null);
   });
+};
