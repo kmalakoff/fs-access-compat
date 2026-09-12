138c173df6f6bbe5579f0df5a753b19335974755
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-11 20:11:06 -0700

    Rename package and fix Node 0.12 compatibility

--- test/spec/index.test.js
+++ test/spec/index.test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 
 var access = require('../..');
 
-describe('fs-access', function () {
+describe('fs-access-compat', function () {
   it('exists', function (done) {
     access(__filename, function (err) {
       assert.ok(!err);
@@ -15,7 +15,7 @@ describe('fs-access', function () {
     var fullPath = __filename + 'junk';
     access(fullPath, function (err) {
       assert.ok(err);
-      assert.equal(err.message.indexOf('ENOENT:'), 0);
+      assert.equal(err.message.indexOf('ENOENT'), 0);
       assert.equal(err.code, 'ENOENT');
       assert.equal(err.errno, -2);
       done();
